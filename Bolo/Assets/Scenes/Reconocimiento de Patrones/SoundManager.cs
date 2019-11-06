e3139152c68c0bda4d0c80778e5c6b5ec6a9ade1
Gabo7 <gabriel-sierra@hotmail.com>
2019-11-05 21:49:46 -0500

    Reconocimiento de Patrones
    5/11

---
+++ Bolo/Assets/Scenes/Reconocimiento de Patrones/SoundManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+
+    [SerializeField]
+    AudioClip[] A;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Bolo/Assets/Scenes/Reconocimiento de Patrones/StageManager.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+[System.Serializable]
+public class Stage
+
+{
+    [SerializeField]
+    public GameObject parent;
+
+    public int[] respuestaCorrecta;
+
+
+}
+
+
+public class StageManager : MonoBehaviour {
+
+    private int currentStage = 0;
+    private int currentClip = 0;
+
+    [SerializeField]
+    public GameObject WinScreen;
+
+    [SerializeField]
+    public GameObject Bolo;
+
+    [SerializeField]
+    public AudioSource AS;
+
+    [SerializeField]
+    public AudioClip[] Sonidos;
+
+    [SerializeField]
+    public GameObject[] Botones;
+
+    [SerializeField]
+    public Stage[] Ejercicios;
+
+    
+
+
+
+    public void CheckAnswer(int x)
+    {
+
+        if(x == Ejercicios[currentStage].respuestaCorrecta[currentClip])
+        {
+            StartCoroutine(PlaySound(currentClip));
+            currentClip++;
+            Debug.Log("correct!");
+            if (currentClip == Ejercicios[currentStage].respuestaCorrecta.Length)
+            {
+                WinScreen.SetActive(true);
+                Ejercicios[currentStage].parent.SetActive(false);
+                Bolo.SetActive(false);
+            }
+        }
+        else
+        {
+            Debug.Log("incorrect!");
+        }
+
+    }
+
+    public void NextChallenge()
+    {
+        WinScreen.SetActive(false);
+        Bolo.SetActive(true);
+        currentStage++;
+        
+        try
+        {
+            Ejercicios[currentStage].parent.SetActive(true);
+            StartCoroutine(PlaySequence());
+        }
+        catch
+        {
+
+        }
+    }
+
+    IEnumerator PlaySound(int x)
+    {
+        yield return null;
+        
+        AS.clip = Sonidos[x];
+        AS.Play();
+
+        while (AS.isPlaying)
+        {
+            yield return null;
+        }
+        
+    }
+
+    IEnumerator PlaySequence()
+    {
+        yield return null;
+
+        foreach (int i in Ejercicios[currentStage].respuestaCorrecta)
+        {
+            AS.clip = Sonidos[i];
+            AS.Play();
+
+            while (AS.isPlaying)
+            {
+                yield return null;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        Ejercicios[currentStage].parent.SetActive(true);
+        StartCoroutine(PlaySequence());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
